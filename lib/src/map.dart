675ee22370d2fdddae319b1ac13cfa7d3cf29d4c
Teh Sunn <tehsunnliu@gmail.com>
2021-12-14 10:11:06 +0530

    Fix location permission prompt

--- lib/src/map.dart
+++ lib/src/map.dart
@@ -106,15 +106,60 @@ class MapPickerState extends State<MapPicker> {
     setState(() => _currentMapType = nextType);
   }
 
+  Future<bool> _determinePosition() async {
+    bool serviceEnabled, permissionGranted = false;
+    LocationPermission permission;
+
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      d('Location services are disabled.');
+      return permissionGranted;
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        d('Location permissions are denied');
+        return permissionGranted;
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      d('Location permissions are permanently denied, we cannot request permissions.');
+      return permissionGranted;
+    }
+
+    return true;
+  }
+
   // this also checks for location permission.
   Future<void> _initCurrentLocation() async {
     Position? currentPosition;
-    try {
-      currentPosition = await Geolocator.getCurrentPosition(
-          desiredAccuracy: widget.desiredAccuracy!);
-      d("position = $currentPosition");
+    bool permissionGranted = await _determinePosition();
 
-      setState(() => _currentPosition = currentPosition);
+    try {
+      if (permissionGranted) {
+        currentPosition = await Geolocator.getCurrentPosition(
+            desiredAccuracy: widget.desiredAccuracy!);
+        d("position = $currentPosition");
+        if (mounted) {
+          setState(() => _currentPosition = currentPosition);
+        }
+      } else {
+        currentPosition = null;
+        d("User denied to grant permission to access location");
+      }
     } catch (e) {
       currentPosition = null;
       d("_initCurrentLocation#e = $e");
